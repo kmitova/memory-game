5085c467f3f39ea839bb1657f56a334b5373ba92
kmitova <96522051+kmitova@users.noreply.github.com>
2022-12-31 16:28:22 +0200

    ordered cards into a grid

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,6 @@ function Card(props) {
   return (
     <div className="card" onClick={() => props.playGame(props.card)}>
       <img src={props.card.src} alt="{props.card.alt}" />
-      <p>{props.card.alt}</p>
     </div>
   );
 }
